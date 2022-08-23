30f063fa4fdcca2996d061c00d6d58cfe9e61301
ShulpinP <pavelshulpin6@gmail.com>
2022-08-23 19:15:15 +0300

    1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -8,38 +8,25 @@ import static java.lang.Integer.parseInt;
 
 
 public class FileReader {
-
-//    File profile = new File("C:\\Users\\Prime_Z370\\IdeaProjects\\stage0-module3-task4\\stage1-module6-io-task1\\src\\main\\resources\\Profile.txt");
-
-    public Profile getDataFromFile(File file) {
+        public Profile getDataFromFile(File file) {
 
         StringBuilder data = new StringBuilder();
-        FileInputStream inputStream = null;
         Map<String,String> map = new HashMap<>();
-        try {
-        inputStream = new FileInputStream("C:\\Users\\Prime_Z370\\IdeaProjects\\stage0-module3-task4\\stage1-module6-io-task1\\src\\main\\resources\\Profile.txt");
-/*        } catch (FileNotFoundException e) {
-            e.printStackTrace();*/
         int ch;
-        while ((ch=inputStream.read()) != -1) {
-            data.append((char) ch);
-        }
-        String[] keyPairs = data.toString().split("\n");
-            for (String pair:keyPairs) {
+        try (FileInputStream inputStream = new FileInputStream("C:\\Users\\Prime_Z370\\IdeaProjects\\stage0-module3-task4\\stage1-module6-io-task1\\src\\main\\resources\\Profile.txt")) {
+            while ((ch = inputStream.read()) != -1) {
+                data.append((char) ch);
+            }
+            String[] keyPairs = data.toString().split("\n");
+            for (String pair : keyPairs) {
                 String[] keyValue = pair.split(": ");
-                map.put(keyValue[0],keyValue[1].trim());
+                map.put(keyValue[0], keyValue[1].trim());
             }
         }
         catch (IOException e) {
             e.printStackTrace();
         }
-        finally {
-            try {
-                inputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+
         return new Profile(map.get("Name"),
                 parseInt(map.get("Age")),
                 map.get("Email"),
