b46846730a04e8d404a64923e15ed4cf3e1e6146
ShulpinP <pavelshulpin6@gmail.com>
2022-08-23 18:54:23 +0300

    1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,48 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.Integer.parseInt;
 
 
 public class FileReader {
 
+//    File profile = new File("C:\\Users\\Prime_Z370\\IdeaProjects\\stage0-module3-task4\\stage1-module6-io-task1\\src\\main\\resources\\Profile.txt");
+
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        StringBuilder data = new StringBuilder();
+        FileInputStream inputStream = null;
+        Map<String,String> map = new HashMap<>();
+        try {
+        inputStream = new FileInputStream("C:\\Users\\Prime_Z370\\IdeaProjects\\stage0-module3-task4\\stage1-module6-io-task1\\src\\main\\resources\\Profile.txt");
+/*        } catch (FileNotFoundException e) {
+            e.printStackTrace();*/
+        int ch;
+        while ((ch=inputStream.read()) != -1) {
+            data.append((char) ch);
+        }
+        String[] keyPairs = data.toString().split("\n");
+            for (String pair:keyPairs) {
+                String[] keyValue = pair.split(": ");
+                map.put(keyValue[0],keyValue[1].trim());
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return new Profile(map.get("Name"),
+                parseInt(map.get("Age")),
+                map.get("Email"),
+                Long.parseLong(map.get("Phone")));
     }
 }
